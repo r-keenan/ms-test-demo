e31275697b0b0a0f3077777c6ee4c539b30f33de
rkeenan <ross.keenan@protonmail.comm>
2024-02-18 16:43:04 -0600

    Added test for invalid email case.

--- Employees.Test/EmployeesTests.cs
+++ Employees.Test/EmployeesTests.cs
@@ -15,4 +15,23 @@ public class EmployeesTests
         Assert.IsNotNull(employee.Name);
         Assert.IsNotNull(employee.Email);
     }
+
+    [TestMethod]
+    public void ValidateEmployee_ShouldFailForEmail()
+    {
+        Employee employee = new Employee();
+        employee.Name = "Tyrone Bigggums";
+        employee.Email = "tBiggumsemail.com";
+        
+        try
+        {
+            Assert.IsNotNull(employee.Name);
+            Assert.IsNotNull(employee.Email);
+        }
+        catch (Exception ex)
+        {
+            Assert.AreEqual("Invalid Email Format", ex.Message);
+            throw;
+        }
+    }
 }
