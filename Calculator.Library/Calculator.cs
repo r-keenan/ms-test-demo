b90d3d92194a8b525c523720027fcc9d1b514eba
rkeenan <ross.keenan@protonmail.comm>
2024-02-18 11:58:36 -0600

    Added attribute note for how to remove something from code coverage.

--- Calculator.Library/Calculator.cs
+++ Calculator.Library/Calculator.cs
@@ -8,4 +8,13 @@ public class Calculator
         Console.WriteLine(result);
         return result;
     }
+    
+    //[System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+    // This attribute is used to remove a method from code coverage results. should be used if the function does not need to be unit tested.
+    // It can be used on more than just methods.
+    public static int Add(int firstNumber, int secondNumber)
+    {
+        int result = firstNumber + secondNumber;
+        return result;
+    }
 }
